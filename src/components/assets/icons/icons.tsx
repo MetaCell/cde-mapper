57d93929d8087b3092974ece739b44cedc48d8a6
emekauja <ujadninth@gmail.com>
2023-08-28 09:54:49 +0100

    added more icons

---
+++ src/components/assets/icons/icons.tsx
@@ -0,0 +1,19 @@
+import { ReactComponent as XIcon } from '../svg/x.svg';
+import { ReactComponent as ArrowLeftIcon } from '../svg/arrow-left.svg';
+import { ReactComponent as ArrowRightIcon } from '../svg/arrow-right.svg';
+import { ReactComponent as ArrowDropdownIcon } from '../svg/arrow-drop-down.svg';
+import { ReactComponent as ArrowDownUpIcon } from '../svg/arrow-down-up.svg';
+import { ReactComponent as CheckIcon } from '../svg/check.svg';
+import { ReactComponent as MagnifyingGlassIcon } from '../svg/magnifying-glass.svg';
+import { ReactComponent as FadingHorizontalIcon } from '../svg/faders-horizontal.svg';
+
+export {
+  XIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowDropdownIcon,
+  ArrowDownUpIcon,
+  CheckIcon,
+  FadingHorizontalIcon,
+  MagnifyingGlassIcon,
+};
