f21aa409cbaffa8f32d3c9bf2e0a7997f7f86429
emekauja <ujadninth@gmail.com>
2023-08-28 09:55:59 +0100

    added custom styled button comp

---
+++ src/components/controls/forms/StyledIconButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import { IconButton, IconButtonProps } from '@mui/material';
+
+interface IStyledIconButton extends IconButtonProps {
+  variant?: 'text' | 'outlined';
+}
+
+const StyledIconButton = styled(IconButton, {
+  shouldForwardProp: prop => prop !== 'variant',
+})<IStyledIconButton>(({ variant, theme }) => ({
+  borderRadius: '0.5rem',
+  ...(variant === 'outlined'
+    ? {
+        border: `1px solid ${theme.palette.grey[300]}`,
+      }
+    : {}),
+}));
+export default StyledIconButton;
