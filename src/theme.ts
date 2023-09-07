bae509bb070fc86d5d616386b6083a3cb732113d
emekauja <ujadninth@gmail.com>
2023-09-07 12:58:15 +0100

    cde-8: chore: added table root style override

--- src/theme.ts
+++ src/theme.ts
@@ -15,6 +15,75 @@ const theme = createTheme({
         },
       },
     },
+    MuiTableCell: {
+      styleOverrides: {
+        stickyHeader: {
+          backgroundColor: palette.grey[50],
+        },
+        root: {
+          '&:not(:last-of-type)': {
+            borderRight: `2px solid ${palette.grey[200]}`,
+          },
+          lineHeight: '1.25rem',
+          color: palette.grey[600],
+          borderCollapse: 'collapse',
+          paddingTop: '0.75em',
+          paddingBottom: '0.75em',
+          paddingInline: '1.5rem',
+          '& span': {
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          },
+          '&:hover': {
+            backgroundColor: palette.grey[50],
+          },
+        },
+      },
+    },
+    MuiTableRow: {
+      styleOverrides: {
+        root: {
+          '&:hover td, &:hover th': {
+            backgroundColor: 'none',
+          },
+        },
+      },
+    },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          '& .MuiTableCell-root': {
+            fontWeight: 600,
+            lineHeight: '1.125rem',
+            padding: '0.75em 1.5em',
+          },
+        },
+      },
+    },
+    MuiAccordionSummary: {
+      styleOverrides: {
+        root: {
+          minHeight: '1.3em',
+
+          '&.Mui-expanded': {
+            minHeight: '1.3em',
+          },
+        },
+        content: {
+          margin: 0,
+          '&.Mui-expanded': {
+            margin: 0,
+          },
+        },
+      },
+    },
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          border: `1px solid ${palette.grey[200]}`,
+        },
+      },
+    },
     MuiButton: {
       styleOverrides: {
         root: {
