52eab7a54ec756591f1396fd14f87b508f6308ed
vidhya-metacell <vidhya@metacell.us>
2023-12-27 16:31:49 +0100

    #CDE-19 Header UI

--- lib/components/layout/ModalLayout.tsx
+++ lib/components/layout/ModalLayout.tsx
@@ -1,51 +1,67 @@
 import React, { PropsWithChildren } from 'react';
-import { Box, Divider, Stack, Typography } from '@mui/material';
-import vars from '../assets/styles/variables';
+import { Box, Divider, Typography, Button,  IconButton} from '@mui/material';
+import { vars } from "../../theme/variables";
+import { CloseIcon } from "../../icons";
+import {useCdeContext} from "../../CdeContext.tsx";
 
+
+const { baseWhite, gray100, gray700 } = vars
+
+const styles = {
+  root: {
+    padding: '0.75rem 1.5rem',
+    gap: '1.5rem',
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: `0.0625rem solid ${gray100}`,
+    background: baseWhite,
+
+    '& .MuiTypography-root': {
+      fontSize: '0.875rem',
+      fontWeight: 600,
+      lineHeight: '142.857%',
+      color: gray700
+    }
+  }
+}
 interface IMainProps {
-    title: string;
-    headerLeftNode?: React.ReactNode;
+    // title: string;
+    // headerLeftNode?: React.ReactNode;
     footerNode?: React.ReactNode;
     footerTopElement?: React.ReactNode;
 }
 
-const { textWhite } = vars;
-
 export const ModalLayout = ({
-                                title,
+                                // title,
                                 children,
-                                headerLeftNode,
+                                // headerLeftNode,
                                 footerNode,
                                 footerTopElement,
                             }: PropsWithChildren<IMainProps>) => {
+    const {handleClose} = useCdeContext();
     return (
         <Box sx={{ width: '100%' }}>
-            <Stack
-                direction="row"
-                spacing={0.75}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    position: 'sticky',
-                    top: 0,
-                    borderBottom: `1px solid grey`,
-                    padding: '0.75rem 1.5rem',
-                    backgroundColor: textWhite,
-                    zIndex: 1,
-                    '& .MuiDivider-root': {
-                        display: 'block',
-                    },
-                    '& .MuiTypography-root': {
-                        fontSize: '0.875rem',
-                        fontWeight: 600,
-                        lineHeight: '1.25rem',
-                    },
-                }}
-            >
-                {headerLeftNode}
-                {headerLeftNode && <Divider />}
-                <Typography>{title}</Typography>
-            </Stack>
+            <Box sx={styles.root}>
+                <Box display='flex' alignItems='center' flex={1}>
+                    <IconButton disableRipple sx={{
+                    borderRadius: '0.5rem',
+
+                    '&:hover': {
+                        background: gray100
+                    }
+                    }}
+                    onClick={handleClose}
+                    >
+                        <CloseIcon />
+                    </IconButton>
+                    <Divider sx={{ borderRight: `0.0625rem solid ${gray100}`, height: '2.25rem', mx: '1rem' }} />
+                    <Typography>
+                        Map selected datasets
+                    </Typography>
+                </Box>
+
+                <Button disableRipple variant="outlined">Info</Button>
+            </Box>
             <Box sx={{ position: 'relative', minHeight: 'calc(100vh - 104px)' }}>
                 {children}
             </Box>
@@ -59,7 +75,7 @@ export const ModalLayout = ({
                         '& .footer': {
                             width: '100%',
                             padding: '0.75rem 1.5rem',
-                            backgroundColor: textWhite,
+                            backgroundColor: baseWhite,
                             borderTop: `1px solid grey`,
                         },
                     }}
