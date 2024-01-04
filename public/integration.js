3f588dbb0473e295d9f83c7da2e60598eaad0154
Zoran Sinnema <zsinnema@gmail.com>
2024-01-04 12:16:03 +0100

    Cde 20 set up test deployment for cde mapper (#10)
    * feat: ci/cd codefresh setup

---
+++ public/integration.js
@@ -0,0 +1,43 @@
+import { init } from '../lib/main.tsx';
+
+export function mapAndInit(cdeFile, datasetFile) {
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        const text = event.target.result;
+
+        Papa.parse(text, {
+            header: true,
+            skipEmptyLines: true,
+            complete: function(results) {
+                const cdeMappings = results.data.map(row => ({
+                    variableName: row['Variable Name (UI)'],
+                    abbreviation: row['Abbreviation'],
+                    interlexID: row['InterLex ID'],
+                    // Map other fields as needed
+                }));
+
+                // Then call the 'init' function from the library
+                init({
+                    inputMappings: [cdeMappings],
+                    datasetSample: datasetFile,
+                    callback: (data) => console.log(data),
+                    repositories: [],
+                    config: { width: '60%', height: '80%' },
+                    labName: 'TestLabName'
+                });
+            },
+            error: function(error) {
+                console.error('Error parsing CSV file:', error.message);
+                // Handle the error as needed
+            }
+        });
+    };
+
+    reader.onerror = function(event) {
+        console.error('Error reading CSV file:', event.target.error);
+        // Handle the error as needed
+    };
+
+    reader.readAsText(cdeFile);
+}
