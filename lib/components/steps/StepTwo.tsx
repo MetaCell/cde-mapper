8a89623ea8134923edf82c246e49df0cdaa6961f
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2024-01-04 12:19:26 +0100

    Feature/cde 15 Suggestion step UI (#8)
    * #CDE-15 Suggestion step UI
    
    * #CDE-15 UI updates
    
    * #CDE-15 Step two update
    
    * #CDE-15 Suggestion detail UI
    
    * #CDE-21 Fix info backdrop issue
    
    * #CDE-15 Button states
    
    ---------
    
    Co-authored-by: Zoran Sinnema <zsinnema@gmail.com>

--- lib/components/steps/StepTwo.tsx
+++ lib/components/steps/StepTwo.tsx
@@ -1,22 +1,127 @@
-import {Stack, Typography} from '@mui/material';
-import {ModalLayout} from "../layout/ModalLayout.tsx";
-import {useCdeContext} from "../../CdeContext.tsx";
+import { Box, Button, Chip, IconButton, Typography } from '@mui/material';
+import { useCdeContext } from "../../CdeContext.tsx";
+import React, { useState } from 'react';
+import { ArrowDropDown, LeftIcon, RightIcon } from '../../icons/index.tsx';
+import SuggestionDetailUI from './SuggestionDetailUI.tsx';
 
+function StepTwo() {
+    const { mapping } = useCdeContext();
 
-function StepOne() {
-    const {mapping} = useCdeContext();
+    const [showOtherSuggestions, setShowOtherSuggestions] = useState(false);
 
     console.log(mapping)
 
     return (
-        <ModalLayout>
-            <Stack alignItems="center" justifyContent="center" width="100%" sx={{mx: 6}}>
-                <Stack spacing={6} sx={{width: 'max-content'}}>
-                    <Typography> This is step 2</Typography>
-                </Stack>
-            </Stack>
-        </ModalLayout>
+        <>
+            <Box overflow='auto' maxHeight='calc(100vh - (3.9375rem + 3.5625rem + 4.4375rem + 2rem + 2rem))' p='1.5rem' pb={6}>
+                <Box mb={3} borderBottom='0.0625rem solid #ECEDEE' py='0.6875rem' display='flex' alignItems='center' justifyContent='space-between'>
+                    <Typography sx={{
+                        color: '#676C74',
+                        fontSize: '0.875rem',
+                        fontWeight: 500,
+                        lineHeight: '142.857%'
+                    }}>
+                        Column header from datasets
+                    </Typography>
+                    <Typography sx={{
+                        color: '#676C74',
+                        fontSize: '0.75rem',
+                        fontWeight: 400,
+                        lineHeight: '150%'
+                    }}>
+                        This is the column header you’re mapping.
+                    </Typography>
+                </Box>
+
+                <Typography sx={{
+                    padding: '0.5rem 0.75rem',
+                    borderRadius: '0.5rem',
+                    border: '0.0625rem solid #E4E5E7',
+                    background: '#F4F5F5',
+                    fontSize: '0.875rem',
+                    color: '#676C74',
+                    lineHeight: '142.857%',
+                    fontWeight: 400,
+                    marginBottom: '3rem'
+                }}>
+                    Strain
+                </Typography>
+
+                <Box mb={3} borderBottom='0.0625rem solid #ECEDEE' py='0.6875rem' display='flex' alignItems='center' justifyContent='space-between'>
+                    <Typography sx={{
+                        color: '#676C74',
+                        fontSize: '0.875rem',
+                        fontWeight: 500,
+                        lineHeight: '142.857%'
+                    }}>
+                        CDE suggestions
+                    </Typography>
+                    <Typography sx={{
+                        color: '#676C74',
+                        fontSize: '0.75rem',
+                        fontWeight: 400,
+                        lineHeight: '150%'
+                    }}>
+                        Select only 1 suggestion to map ‘Strain’ with. Suggestions are ordered based on the quality of the suggestion.
+                    </Typography>
+                </Box>
+
+                <Box display='flex' alignItems='start' flexDirection='column' gap='3rem'>
+                    <SuggestionDetailUI />
+                    <SuggestionDetailUI />
+                    <Button variant='text' onClick={() =>  setShowOtherSuggestions(!showOtherSuggestions)} disableRipple sx={{
+                        p: 0, gap: '0.25rem', color: '#19418F',
+
+                        '& svg': {
+                            transform: showOtherSuggestions ? 'rotate(90deg)' : 'rotate(0deg)'
+                        }
+                    }}>
+                        <ArrowDropDown />
+                        4 other suggestions available for this column. Expand all suggestions.
+                    </Button>
+                </Box>
+
+
+                {showOtherSuggestions && (
+                    <Box display='flex' alignItems='start' flexDirection='column' mt='3rem' gap='3rem'>
+                        <SuggestionDetailUI />
+                        <SuggestionDetailUI />
+                        <SuggestionDetailUI />
+                        <SuggestionDetailUI />
+                    </Box>
+                )}
+            </Box>
+
+            <Box width='calc(100% - 3rem)' mx='auto' justifyContent='space-between' display='flex' alignItems='center' sx={{ background: '#fff', zIndex: 9 }} py='1rem' borderTop='0.0625rem solid #ECEDEE'>
+                <Box gap='0.75rem' display='flex' alignItems='center'>
+                    <Box gap='0.25rem' display='flex' alignItems='center'>
+                        <IconButton disabled sx={{
+                            borderRadius: '0.5rem',
+                            padding: '0.5rem',
+                            border: '0.0625rem solid #E4E5E7',
+                            boxShadow: '0rem 0.0625rem 0.125rem 0rem rgba(7, 8, 8, 0.05)'
+                        }}>
+                            <LeftIcon color="#D6D8DB" />
+                        </IconButton>
+                        <IconButton sx={{
+                            borderRadius: '0.5rem',
+                            padding: '0.5rem',
+                            border: '0.0625rem solid #D6D8DB',
+                            boxShadow: '0rem 0.0625rem 0.125rem 0rem rgba(7, 8, 8, 0.05)'
+                        }}>
+                            <RightIcon />
+                        </IconButton>
+                    </Box>
+                    <Chip label='Displaying 1/3 suggestions' color='primary' />
+                </Box>
+
+                <Box gap='0.5rem' display='flex' alignItems='center'>
+                    <Button variant='outlined'>Ignore suggestions</Button>
+                    <Button variant='contained'>Accept selected mapping</Button>
+                </Box>
+            </Box>
+        </>
     );
 }
 
-export default StepOne;
+export default StepTwo;
--- lib/theme/index.tsx
+++ lib/theme/index.tsx
@@ -1,7 +1,8 @@
 import { createTheme } from "@mui/material/styles";
 import { vars } from "./variables";
+import CaretRight from '../images/CaretRight.png';
 
-const { primaryFont, gray300, baseWhite, gray500 } = vars
+const { primaryFont, gray300, gray50, baseWhite, gray500, primary700, gray700, primary600, primary100, black } = vars
 
 let theme = createTheme();
 
@@ -15,13 +16,13 @@ theme = createTheme({
       fontSize: '1.5rem',
       fontWeight: 500,
       lineHeight: '150%',
-      color: '#373A3E',
+      color: gray700,
     },
     body2: {
       fontSize: '1rem',
       fontWeight: 400,
       lineHeight: '150%',
-      color: '#676C74',
+      color: gray500,
     }
   },
 
@@ -60,6 +61,14 @@ theme = createTheme({
       }
     },
 
+    MuiDialog: {
+      styleOverrides: {
+        paperWidthXl: {
+          maxWidth: '68.75rem'
+        }
+      }
+    },
+
     MuiDrawer: {
       styleOverrides: {
         root: {
@@ -68,6 +77,14 @@ theme = createTheme({
         paper: {
           width: '22.5rem',
           boxShadow: '0rem 0.5rem 0.5rem -0.25rem rgba(16, 24, 40, 0.03), 0rem 1.25rem 1.5rem -0.25rem rgba(16, 24, 40, 0.08)'
+        }
+      }
+    },
+
+    MuiTouchRipple: {
+      styleOverrides: {
+        root: {
+          display: 'none',
         }
       }
     },
@@ -90,18 +107,27 @@ theme = createTheme({
         },
 
         containedPrimary: {
-          background: '#19418F',
+          background: primary600,
           '&:hover': {
-            background: '#19418F',
+            background: primary700,
+          },
+          '&:focus': {
+            background: primary600,
+            boxShadow: '0rem 0rem 0rem 0.25rem #C2D4F4, 0rem 0.0625rem 0.125rem 0rem rgba(7, 8, 8, 0.05)'
+          },
+          '&.Mui-disabled': {
+            background: primary100,
+            color: baseWhite,
           }
         },
 
         textPrimary: {
-          color: '#676C74',
+          color: gray500,
           boxShadow: 'none',
           '&:hover': {
             boxShadow: 'none',
-            background: 'transparent'
+            color: gray700,
+            background: gray50
           }
         },
 
@@ -111,12 +137,114 @@ theme = createTheme({
           color: gray500,
           '&:hover': {
             border: `0.0625rem solid ${gray300}`,
-            background: baseWhite,
+            background: gray50,
             color: gray500,
+          },
+          '&:focus': {
+            border: `0.0625rem solid ${gray300}`,
+            background: baseWhite,
+            boxShadow: '0rem 0rem 0rem 0.25rem #ECEDEE, 0rem 0.0625rem 0.125rem 0rem rgba(7, 8, 8, 0.05)'
+          },
+          '&.Mui-disabled': {
+            background: baseWhite,
+            border: `0.0625rem solid #E4E5E7`,
+            color: gray300,
           }
         }
       }
-    }
+    },
+
+    MuiTooltip: {
+      styleOverrides: {
+        tooltip: {
+          backgroundColor: black,
+          padding: '0.75rem',
+          borderRadius: '0.5rem'
+        }
+      }
+    },
+
+    MuiTabs: {
+      styleOverrides: {
+        root: {
+          padding: 0
+        },
+        indicator: {
+          backgroundColor: primary600
+        }
+      }
+    },
+
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          padding: 0
+        }
+      }
+    },
+
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          height: 'auto',
+          fontFamily: primaryFont,
+          borderRadius: '1rem',
+          padding: '0.125rem 0.5rem',
+        },
+        label: {
+          fontSize: '0.75rem',
+          lineHeight: '150%',
+          fontWeight: 500,
+          padding: 0,
+          fontFamily: primaryFont,
+        },
+        filledPrimary: {
+          background: '#EEF2FC',
+          '& .MuiChip-label': {
+            color: '#2155BA'
+          }
+        }
+      }
+    },
+
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          flexDirection: 'row',
+          minHeight: '3.375rem',
+          overflow: 'visible',
+          textTransform: 'none',
+          fontSize: '0.875rem',
+          columnGap: '0.5rem',
+          fontWeight: 500,
+          lineHeight: '142.857%',
+          color: gray500,
+          padding: '1.125rem 0',
+
+          '&.Mui-selected': {
+            color: primary600,
+            fontWeight: 600
+          },
+
+          '&:not(:first-of-type)': {
+            marginLeft: '2.5rem',
+            position: 'relative',
+
+            '&:before': {
+              content: '""',
+              backgroundImage: `url(${CaretRight})`,
+              left: '-1.75rem',
+              backgroundPosition: 'center',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              width: '1rem',
+              height: '1rem',
+              position: 'absolute'
+            }
+          }
+        }
+      }
+    },
   }
 });
 
