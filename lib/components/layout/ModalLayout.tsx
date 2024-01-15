7d2ea93953a334d97a80fe4329c4f0ad83e6504b
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2024-01-15 12:40:07 +0100

    CDE-29 create common circular progress component

--- lib/components/layout/ModalLayout.tsx
+++ lib/components/layout/ModalLayout.tsx
@@ -1,8 +1,8 @@
 import React, { PropsWithChildren } from 'react';
-import { Box, Typography, CircularProgress} from '@mui/material';
 // import { vars } from "../../theme/variables";
 import {useCdeContext} from "../../CdeContext.tsx";
 import Header from "../common/Header";
+import { CommonCircularProgress } from '../common/CommonCircularProgress.tsx';
 
 // const { baseWhite } = vars
 
@@ -21,10 +21,7 @@ export const ModalLayout = ({
     <>
       <Header />
       {loadingMessage ? (
-        <Box sx={{ background: '#FCFCFD' }} py={25} flexDirection='column' display='flex' alignItems='center'>
-          <CircularProgress />
-          <Typography sx={{fontSize: '0.875rem', fontWeight: 400, lineHeight: '142.857%', mt: '0.75rem', color: '#676C74'}}>Processing data...</Typography>
-        </Box>
+        <CommonCircularProgress label='Processing data...'/>
       ) : children}
       {/* {(footerNode || footerTopElement) && (
         <Box
