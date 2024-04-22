53337a9a10498ab07b2f50e82de75b1b81b70953
Simao Bolota <simao@metacell.us>
2024-04-22 20:03:04 +0100

    CDE-42 working on test steps from dataset mapping

--- tests/e2e/tests/DatasetMapping.test.ts
+++ tests/e2e/tests/DatasetMapping.test.ts
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 
 const URL = process.env.url || "https://cde-mapper.dev.metacell.us/";
-const TIMEOUT = 60000;
+const TIMEOUT = 6000;
 
 
 //SNAPSHOT:
@@ -116,14 +116,103 @@ describe('Control Panel Test', () => {
         })
     })
 
-    describe('Go through Suggestions', () => { 
-        test('Accept 1/3 suggestion', async () => {})
-        test('Ignore 2/3 suggestion', async () => {})
-        test('Accept 3/3 suggestion', async () => {})
+    describe('Go through Suggestions', () => {
+        test('Accept 1/3 suggestion', async () => {
+            await dm_test_page.waitForSelector('.suggestion-details > div > span', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.suggestion-details > div > span');
+            await dm_test_page.waitForSelector('.suggestions__navigation-block > div > button:nth-child(2)');
+            await dm_test_page.click('.suggestions__navigation-block > div > button:nth-child(2)');
+        })
+        test('Ignore 2/3 suggestion', async () => {
+            await dm_test_page.waitForSelector('.suggestions__button-block > button:nth-child(1)', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.suggestions__button-block > button:nth-child(1)');
+            await dm_test_page.waitForSelector('.suggestions__navigation-block > div > button:nth-child(2)');
+            await dm_test_page.click('.suggestions__navigation-block > div > button:nth-child(2)');
+        })
+        test('Ignore 3/3 suggestion', async () => {
+            await dm_test_page.waitForSelector('.suggestions__button-block > button:nth-child(1)', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.suggestions__button-block > button:nth-child(1)');
+            await dm_test_page.waitForSelector('.suggestions__button-block > button:nth-child(2)', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.suggestions__button-block > button:nth-child(2)');
+            await dm_test_page.waitForSelector('.mapping-step', { hidden: false, timeout: TIMEOUT });
+        })
 
     })
 
-    describe('Map Dataset', () => { })
+    describe('Map Dataset', () => {
+
+        test('Sort fields', async () => {
+            await dm_test_page.waitForSelector('.mapping__sort-icon', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.mapping__sort-icon');
+            await dm_test_page.waitForTimeout(500);
+            await dm_test_page.click('.mapping__sort-icon');
+            const elements_after_sort = await dm_test_page.$$eval('.mapping-chip', nodes => nodes.map(n => n.innerText));
+            for (let i = 0; i < 5; i++) {
+                expect(elements_after_sort[i]).toBe('Unmapped');
+            }
+        })
+        test('Filter fields', async () => {
+            await dm_test_page.waitForSelector('.mapping__filter-btn', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.mapping__filter-btn');
+            await dm_test_page.waitForSelector('.mapping__filter-popover', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.waitForSelector('.mapping__filter-popover > div > div > div > label:nth-child(3)', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.mapping__filter-popover > div > div > div > label:nth-child(3)');
+            await dm_test_page.mouse.click(0, 0);
+            const elements_after_filter = await dm_test_page.$$('.mapping-chip');
+            expect(elements_after_filter.length).toBe(5);
+
+        })
+        test('Map fields', async () => {
+            await dm_test_page.waitForFunction(
+                () => (document.querySelector('.mapping-header__indicator') as HTMLElement).innerText === '5/13 column headers still unmapped',
+                { timeout: TIMEOUT }
+            );
+            await dm_test_page.waitForSelector('#Species .cde-fields__item-first div', { hidden: false, timeout: TIMEOUT });
+            // await dm_test_page.click('#Species .cde-fields__item-first div');
+            await dm_test_page.waitForTimeout(500);
+            await dm_test_page.click('#Species .cde-fields__item-first div');
+            await dm_test_page.waitForSelector('input[placeholder="Search in multiple collections"]', { hidden: false, timeout: TIMEOUT })
+            await dm_test_page.waitForSelector('.cde-field__popper li', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.cde-field__popper li');
+
+            await dm_test_page.waitForSelector('input[placeholder="Search in multiple collections"]', { hidden: false, timeout: TIMEOUT })
+            await dm_test_page.waitForSelector('.cde-field__popper li', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.cde-field__popper li');
+
+            await dm_test_page.waitForSelector('input[placeholder="Search in multiple collections"]', { hidden: false, timeout: TIMEOUT })
+            await dm_test_page.waitForSelector('.cde-field__popper li', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.cde-field__popper li');
+
+            await dm_test_page.waitForSelector('input[placeholder="Search in multiple collections"]', { hidden: false, timeout: TIMEOUT })
+            await dm_test_page.waitForSelector('.cde-field__popper li', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.cde-field__popper li');
+
+            await dm_test_page.waitForSelector('input[placeholder="Search in multiple collections"]', { hidden: false, timeout: TIMEOUT })
+            await dm_test_page.waitForSelector('.cde-field__popper li', { hidden: false, timeout: TIMEOUT });
+            await dm_test_page.click('.cde-field__popper li');
+
+        })
+
+        test('Check fields', async () => {
+
+            await dm_test_page.waitForFunction(
+                () => (document.querySelector('.mapping-header__indicator') as HTMLElement).innerText === '0/13 column headers still unmapped',
+                { timeout: TIMEOUT }
+            );
+
+            // await dm_test_page.waitForSelector('.mapping__filter-btn', { hidden: false, timeout: TIMEOUT });
+            // await dm_test_page.click('.mapping__filter-btn');
+            // await dm_test_page.waitForSelector('.mapping__filter-popover', { hidden: false, timeout: TIMEOUT });
+            // await dm_test_page.waitForSelector('.mapping__filter-popover > div > div > div > label:nth-child(3)', { hidden: false, timeout: TIMEOUT });
+            // await dm_test_page.click('.mapping__filter-popover > div > div > div > label:nth-child(3)');
+            await dm_test_page.waitForSelector('.mapping-chip', { hidden: false, timeout: TIMEOUT });
+            const elements_after_mapping = await dm_test_page.$$eval('.mapping-chip', nodes => nodes.map(n => n.innerText));
+            for (let i = 0; i < 12; i++) {
+                expect(elements_after_mapping[i]).toBe('Mapped to CDE');
+            }
+        })
+
+    })
 
     describe('Download Dataset', () => { })
 
