5fbe2bd770f9c9c380db72d31f84a43748634afe
Simao Bolota <simao@metacell.us>
2024-06-03 16:56:45 +0100

    headless mode

--- tests/e2e/tests/TemplateCreation.test.ts
+++ tests/e2e/tests/TemplateCreation.test.ts
@@ -27,7 +27,7 @@ describe('CDE: Template Creation Test', () => {
     beforeAll(async () => {
         tc_test_browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox', "--ignore-certificate-errors"],
-            headless: false,
+            headless: true,
             devtools: false,
             defaultViewport: {
                 width: 1600,
